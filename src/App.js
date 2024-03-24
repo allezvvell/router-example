0c0e7ff1e6db98c3680d22c31d50a5e3db3786d4
김세현 <allezvvell@gmail.com>
2024-03-24 23:02:57 +0900

    import경로 수정

--- src/App.js
+++ src/App.js
@@ -1,16 +1,16 @@
 import './App.css';
 import { Routes, Route} from 'react-router-dom';
 import Home from './page/Home';
-// import About from './page/About';
-import Product from './page/About';
-import ProductDetail from './page/About';
+import About from './page/About';
+import Product from './page/Product';
+import ProductDetail from './page/ProductDetail';
 
 function App() {
   return (
     <div className="App">
       <Routes>
         <Route path='/' element={<Home />} />
-        {/* <Route path='/about' element={<About />} /> */}
+        <Route path='/about' element={<About />} />
         <Route path='/products' element={<Product />} />
         <Route path='/detail' element={<ProductDetail />} />
       </Routes>
