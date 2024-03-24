c1fd9f04d9f0dea882218a37c48a9d5718fd6859
김세현 <allezvvell@gmail.com>
2024-03-24 22:12:23 +0900

    2nd

--- src/page/Home.js
+++ src/page/Home.js
@@ -5,8 +5,9 @@ const Home = () => {
   return (
     <div>
       <h1>Home Page</h1>
-      <Link to='/about'>go to About</Link>
-      <Link to='/products'>go to products</Link>
+      <Link to='/about'>go to About</Link><br/>
+      <Link to='/products'>go to products</Link><br/>
+      <Link to='/details'>go to datails</Link><br/>
     </div>
   )
 }
